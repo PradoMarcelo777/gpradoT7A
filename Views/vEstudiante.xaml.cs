173cf218717409a3efe1d05c437caa2d76e03c14
PradoMarcelo777 <geotzmprado777@outlook.com>
2024-11-09 01:39:12 -0500

    Agregar archivos de proyecto.

---
+++ Views/vEstudiante.xaml.cs
@@ -0,0 +1,31 @@
+using gpradoT7A.Models;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+
+namespace gpradoT7A.Views;
+
+public partial class vEstudiante : ContentPage
+{
+    private const string Url = "http://localhost:8081/uisraelws/estudiante.php";
+    private readonly HttpClient cliente = new HttpClient();
+
+    //la lista en la interfaz de usuario se actualizara automaticamente cuando se agregue o elimines un estudiante.
+    private ObservableCollection<Estudiante> todosLosEstudiantes;
+
+    public vEstudiante()
+	{
+		InitializeComponent();
+        mostrar();
+    }
+
+    //Agregar paquete Nugets Newtonsoft.Json
+    public async void mostrar()
+    {
+        var datosEstudiantesJson = await cliente.GetStringAsync(Url); // Obtiene los datos JSON de la URL
+        List<Estudiante> mostrarEstudiantes = JsonConvert.DeserializeObject<List<Estudiante>>(datosEstudiantesJson); // Deserializa el JSON a una lista de estudiantes
+        //Console.WriteLine(mostrarEstudiantes);
+        todosLosEstudiantes = new ObservableCollection<Estudiante>(mostrarEstudiantes); // Convierte la lista en ObservableCollection
+        listaEstudiantes.ItemsSource = todosLosEstudiantes; // Asigna la coleccion a la fuente de datos de la lista en la UI
+
+    }
+}
